5aba3976130a367919f63d615151b3a6872dc409
Jonatan Guinea <jonatanguinea@outlook.com>
2024-10-27 20:22:22 -0300

    configurando ProductManager

--- src/app.js
+++ src/app.js
@@ -1,35 +1,38 @@
 import express from "express"
-import { __dirname } from "./utils.js"
+import  mongoose  from "mongoose";
 import handlebars from "express-handlebars"
 import {Server} from "socket.io"
-import { config } from "./utils.js"
+
+
 import routerP from "./routes/products.router.js";
 import routerV from "./routes/views.router.js";
 import socketProducts from "./listeners/socketproducts.js";
-import  mongoose  from "mongoose";
+import { config } from "./utils.js"
+import { __dirname } from "./utils.js"
 
 
 const app = express()
 
-app.use(express.static(__dirname + "/public"))
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-//handlebars
-app.engine("handlebars",handlebars.engine())
-app.set("views", __dirname+"/views")
-app.set("view engine","handlebars")
-//rutas
-app.use("/api",routerP)
-app.use('/', routerV);
 
 
 const httpServer=app.listen(config.PORT,async () => {
 
     await mongoose.connect(config.MONGODB_URI)
 
+    
+    app.use(express.static(__dirname + "/public"))
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    //handlebars
+    app.engine("handlebars",handlebars.engine())
+    app.set("views", __dirname+"/views")
+    app.set("view engine","handlebars")
+    //rutas
+    app.use("/api",routerP)
+    app.use('/', routerV);
+    
+    
     console.log(`Server activo en puerto ${config.PORT}, conectado a bbdd`);
-    
-    
 });
 
 const socketServer = new Server(httpServer)
