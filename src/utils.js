09e3e40b9a73af6906dd8f2a112c49735717f1ab
Jonatan Guinea <jonatanguinea@outlook.com>
2024-11-15 19:39:38 -0300

    Corrigiendo PORT

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@ export  const __dirname=dirname(fileURLToPath(import.meta.url))
 
 
 export const config = {
-   PORT: 'tp2-coderhouse-reentrega.vercel.app',
+   PORT: 3000,
    MONGODB_URI: 'mongodb+srv://jonatanguinea7:642859Jj642859@cluster0.mesld.mongodb.net/products',
 
       
