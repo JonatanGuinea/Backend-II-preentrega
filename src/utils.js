cad78bd40add836f2358129cc49583ec238a317e
Jonatan Guinea <jonatanguinea@outlook.com>
2024-11-15 19:45:40 -0300

    Configurando para vercel

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@ export  const __dirname=dirname(fileURLToPath(import.meta.url))
 
 
 export const config = {
-   PORT: 3000,
+   PORT: process.env.PORT|| 3000,
    MONGODB_URI: 'mongodb+srv://jonatanguinea7:642859Jj642859@cluster0.mesld.mongodb.net/products',
 
       
